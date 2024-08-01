2bb765c1f918b3ed1d0952ee9d9f3b50e6dd22c3
Facundo Ferrari <f.ferrari@thebluebox.dev>
2024-08-01 20:28:09 -0300

    first stable version

--- cache.h
+++ cache.h
@@ -11,81 +11,69 @@ class CacheManager {
     int capacity;
     int current_mru;
 
-    map <string, pair<T, int>> cache_data; //<Clave ,<Obj, Indice de Uso >>
+    map<string, pair<T, int>> cache_data; //<Clave ,<Obj, Indice de Uso >>
+    map<string, T> file_data;           //Copy of file data
 
     T default_value;
     fstream disk;
 
-    bool open_file();
-    bool write_file(string ,T);
+    void write_file(string, T);
 
     public:
         CacheManager(int); // recibe la capacidad en el int
-        ~CacheManager ();
-        void insert(string key , T obj);
+        ~CacheManager();
+        void insert(string key, T obj);
         T get(string key);
-
         void show_cache();
 
-        // Agregar todas las funciones necesarias
+        //File management
+        void load_file();
+        void show_file_copy();
 };
 
 template <class T>
-CacheManager <T>::CacheManager(int cap) {
+CacheManager<T>::CacheManager(int cap){
     capacity = cap;
     current_mru = 0;
+    // // Abrir el archivo al construir el objeto
+    // disk.open("disk.txt", ios::in | ios::out | ios::app);
+    // if (!disk.is_open()) {
+    //     cerr << "No se pudo abrir el archivo para lectura/escritura." << endl;
+    // }
 }
 
 template <class T>
-CacheManager <T>::~CacheManager () {
-    disk.close();
-}
-
-template<class T>
-bool CacheManager <T>::open_file() {
-    string filename = "disk.txt";
-
-    disk.open(filename, ios::app);
-    if (ios::goodbit == 0)
-        return true;
-    else
-        return false;
-}
-
-template <class T>
-bool CacheManager <T>::write_file(string key , T obj) {
-    if (!disk.is_open())
-        open_file();
-    else
-        disk << key << ' ' << obj;
-    return true;
+CacheManager<T>::~CacheManager() {
+    // Cerrar el archivo al destruir el objeto
+    if (disk.is_open()) {
+        disk.close();
+    }
 }
 
 // INSERT
 template <class T>
-void CacheManager <T>::insert(string key , T obj) {
-    write_file(key,obj);
-    if (cache_data.count(key)){ //Existe
+void CacheManager<T>::insert(string key, T obj) {
+    if (cache_data.count(key)) { // Existe
+        cout << "Existe en RAM.\n";
         cache_data[key] = make_pair(obj, current_mru++);
-        return;
+    } else { // No existe
+        if (cache_data.size() == capacity) { // No hay espacio
+            auto mru = max_element(cache_data.begin(), cache_data.end(), [](const pair<string, pair<T, int>>& a, const pair<string, pair<T, int>>& b) {
+                return a.second.second < b.second.second;
+            });
+            cache_data.erase(mru->first);
+        }
+        cache_data[key] = make_pair(obj, current_mru++);
     }
-    //No existe
-    if (cache_data.size() == capacity) // No hay espacio
-    {
-        auto mru = max_element(cache_data.begin(), cache_data.end(), [](const pair<string, pair<T, int>>& a,const pair<string, pair<T, int>>& b) {
-            return a.second.second < b.second.second;
-        });
-        cache_data.erase(mru->first);
-    }
-    cache_data[key] = make_pair(obj, current_mru++);
+    write_file(key, obj);
 }
 
 template <class T>
-T CacheManager <T>::get(string key) {
-    if(cache_data.count(key))
-        return cache_data[key].first;   //encontrado
+T CacheManager<T>::get(string key) {
+    if (cache_data.count(key))
+        return cache_data[key].first; // encontrado
     else
-        return default_value;   //no encontrado
+        return default_value; // no encontrado
 }
 
 template <class T>
@@ -98,5 +86,72 @@ void CacheManager <T>::show_cache() {
         iter->second.first.print();
         cout << "]\n";
         iter++;
+    }
+}
+
+//File management classes
+
+template <class T>
+void CacheManager <T>::load_file() {
+    string line;
+
+    if (disk.is_open()) {
+        disk.clear();
+        disk.seekg(0, ios::beg);
+    } else {
+        disk.open("disk.txt", ios::in | ios::out);
+        if (!disk.is_open()) {
+            cerr << "No se pudo abrir el archivo para lectura/escritura." << endl;
+            return;
+        }
+    }
+
+    while(getline(disk, line)){
+        if(line == "")
+            break;
+        int space_pos = line.find(' ');
+        string line_key = line.substr(0, space_pos);
+        string line_obj = line.substr(space_pos + 1);       
+        file_data[line_key] = T(line_obj);
+    }
+}
+
+template <class T>
+void CacheManager <T>::show_file_copy() {
+    
+    if (!disk.is_open()) {
+        cerr << "Documento no abierto. " << endl;
+        return;
+    }
+    cout << "Copia del archivo: " << endl;
+    auto iter = file_data.begin();
+    while (iter != file_data.end()) {
+        cout << "[" << iter->first << ",";
+        iter->second.print();
+        cout << "]\n";
+        iter++;
+    }
+}
+
+// Maneja la inserción y modificación de los datos
+template <class T>
+void CacheManager<T>::write_file(string key, T obj) {
+
+    cout << "Write file: " << key << ' ' << obj << endl;
+    auto file_data_it = file_data.begin();
+    disk.clear();
+
+    if(file_data.count(key) > 0) {
+        cout << "SE ENCONTRO" << endl;
+        disk.seekp(0, ios::beg);
+        file_data[key] = obj;
+        while (file_data_it != file_data.end()){
+            cout << "First: " << file_data_it->first << "f " << file_data_it->second << endl;
+            disk << file_data_it->first << ' ' << file_data_it->second << endl;    
+            file_data_it ++;
+        }
+    } else {
+        disk.seekp(0, ios::end);
+        disk << key << ' ' << obj << endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,49 +1,62 @@
 #include <string>
 #include <iostream>
 #include "cache.h"
+#include <sstream>
 
 using namespace std;
 
 class Student{
     int id;
     int value;
-    const char* data;
+    string data;
 
     public:
         static const string class_name; //Cada clase tiene un static llamado: class_name
         Student() : id(0), value(0), data("") {} // Constructor predeterminado
-        Student(int _key , int _value , const char* _data): id(_key), value(_value), data(_data) {}
-
+        Student(int _key , int _value , string _data): id(_key), value(_value), data(_data) {}
+        Student(string);
         friend ostream& operator <<(ostream&, Student );
         void print () {
-            cout <<"Student Object: "<<id<<", "<<value <<", "<<data<<endl;
+            cout <<"Student Object: ("<<id<<", "<<value <<", "<<data<<")";
         };
         
         ~Student (){};
 };
 
+Student::Student(string student_string) {
+
+    string val_str;
+
+    stringstream ss(student_string);
+
+    vector<string> values;
+
+    while (getline(ss,val_str,' ')) {
+        values.push_back(val_str);
+    }
+    id = stoi(values[0]);
+    value = stoi(values[1]);
+    data = values[2];
+}
+
 ostream& operator <<(ostream& os, Student student) {
-    os << "Student("<<student.id<<","<<student.value<<","<<student.data<<")"<<endl;
+    os << student.id << ' ' << student.value << ' ' << student.data << ' ';
     return os;
 }
 
 const string Student :: class_name = "StudentClass";
 
 int main() {
-    CacheManager<Student> my_cache(1);
-
-    my_cache.insert("0", Student(0,22,"student1"));
-    my_cache.insert("1", Student(0,22,"student1"));
-    // my_cache.insert("1", Student(1,23,"student2"));
-    // my_cache.insert("2", Student(2,25,"student3"));
-    // my_cache.insert("3", Student (3,29,"student4"));
-
+    CacheManager<Student> my_cache(3);
+    my_cache.load_file();
+    my_cache.show_file_copy();
+    // my_cache.insert("0", Student(0,22,"student22"));
+    my_cache.insert("37", Student(1,23,"studiante"));
+    // my_cache.insert("0", Student(2,24,"student4"));
+    // my_cache.insert("2", Student(2,25,"student5"));
+    // my_cache.insert("3", Student(3,29,"student4"));
+    cout << "\nCache snapshot: \n";
     my_cache.show_cache();
-    
-    cout << "Resultado 1: ";
-    my_cache.get("1").print();
-    cout << "Resultado 2: "; 
-    my_cache.get("0").print();
 
     // cout << "---------------------- UPDATE----------------------- " << endl;
     
@@ -63,6 +76,5 @@ int main() {
     // my_cache.insert("9", Student(1,5,"Nine"));
     // my_cache.insert("9", Student(1,5,"Nine"));
     // my_cache.show_cache ();
-
     return 0;
 }
